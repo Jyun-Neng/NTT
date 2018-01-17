eb53d070575fd9551e5844103e6c271b6859d409
Jyun-NengJi <jyunnengji@gmail.com>
2018-01-17 20:59:15 +0800

    NTT algorithm test

--- NTT_test.py
+++ NTT_test.py
@@ -1,13 +1,28 @@
+import numpy as np
 from NTT import NTT
 
 ntt = NTT()
 M = 2013265921
 N = 2048
+r = ntt.NthRootOfUnity(M, N)
 print("Modulus is %d" % M)
 print("The number of FFT points is %d" % N)
-r = ntt.NthRootOfUnity(M, N)
 print("The %dth root of unity is %d" % (N, r))
 if ntt.isNthRootOfUnity(M, N, r):
     print("%d to power of %d is congruent to 1 modulo %d" % (r, N, M))
 else:
     print("%d to power of %d is not congruent to 1 modulo %d" % (r, N, M))
+
+
+M = 2013265921
+poly = [1, 2, 3, 4]     # 4x^3+3x^2+2x+1
+N = len(poly)
+w = ntt.NthRootOfUnity(M, N)
+ntt_poly = ntt.ntt(poly, M, N, w)
+intt_poly = ntt.intt(ntt_poly, M, N, w)
+print("Modulus : %d" % M)
+print("Polynomial : ", poly)
+print("Polynomial degree : %d" % (N - 1))
+print("Primitive %dth root of unity : %d" % (N, w))
+print("NTT(poly) = ", ntt_poly)
+print("Inverse NTT : ", intt_poly)
